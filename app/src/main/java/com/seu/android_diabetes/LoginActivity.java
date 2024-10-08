1529e60006fc98773965b668fff3b9a47239981f
seanwong <756808193@qq.com>
2024-10-09 00:16:40 +0800

    hotfix2

--- app/src/main/java/com/seu/android_diabetes/LoginActivity.java
+++ app/src/main/java/com/seu/android_diabetes/LoginActivity.java
@@ -67,7 +67,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
 
-        //获取手机号和密码
 
         username_editText=findViewById(R.id.et_phone_number);
         password_editText=findViewById(R.id.et_verify_code);
